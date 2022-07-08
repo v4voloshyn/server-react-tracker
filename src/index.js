79e054f889afd3f0e20947b7ff847783ffa1c9f6
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-07-08 17:38:22 +0300

    delete Redux Persist

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import './index.css';
 
-import { persistor, store } from './store/store';
-
 import App from './App';
-import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
-		<PersistGate loading={<h2>LOADING....</h2>} persistor={persistor}>
-			<App />
-		</PersistGate>
+		<App />
 	</Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,43 +1,7 @@
-import {
-	FLUSH,
-	PAUSE,
-	PERSIST,
-	PURGE,
-	REGISTER,
-	REHYDRATE,
-	persistReducer,
-	persistStore,
-} from 'redux-persist';
-
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './rootReducer';
-import storage from 'redux-persist/lib/storage';
-
-const persistConfig = {
-	key: 'root',
-	version: 1,
-	storage,
-	blacklist: ['track'],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-	reducer: persistedReducer,
+	reducer: rootReducer,
 	devTools: true,
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-			},
-		}),
 });
-
-//  let persistor = persistStore(store)
-
-// export const store = configureStore({
-// 	reducer: persistedReducer,
-// 	devTools: true,
-// });
-
-export const persistor = persistStore(store);
