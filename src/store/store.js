187431ea86bc1d7638ff7ffd3105c59911124201
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-07-08 20:12:26 +0300

    Off devTools in Production Mode

--- src/store/store.js
+++ src/store/store.js
@@ -3,5 +3,5 @@ import { rootReducer } from './rootReducer';
 
 export const store = configureStore({
 	reducer: rootReducer,
-	devTools: true,
+	devTools: process.env.NODE_ENV !== 'production',
 });
