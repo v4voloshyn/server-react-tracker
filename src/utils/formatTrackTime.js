f23bff2cdb4815988e1dedc98806631cc8a03f4a
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-07-10 13:15:33 +0300

    add time formatter

---
+++ src/utils/formatTrackTime.js
@@ -0,0 +1,18 @@
+export const formatTrackTime = (countInSeconds) => {
+	let h = Math.floor(countInSeconds / 3600);
+	let m = Math.floor((countInSeconds / 60) % 60);
+	let s = countInSeconds - h * 3600 - m * 60;
+	if (h < 10) {
+		h = '0' + h;
+	}
+	if (m < 10) {
+		m = '0' + m;
+	}
+	if (s < 10) {
+		s = '0' + s;
+	}
+
+	return `${h}:${m}:${s}`; // Return is HH : MM : SS
+};
+
+console.log(formatTrackTime(3725));
