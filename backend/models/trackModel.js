049f59afda5871aaed3e8b93d7e3317fa6c7b46e
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-10-17 23:25:07 +0300

    Feat: add Track Schema

---
+++ backend/models/trackModel.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const trackSchema = mongoose.Schema({
+	title: {
+		type: String,
+		required: [true, 'Please enter a name of track']
+	},
+	secondsCount: {
+		type: Number,
+		default: 0,
+	},
+	isPaused: {
+		type: Boolean,
+		default: false
+	},
+}, {
+	timestamps: true,
+});
+
+export default mongoose.model('track', trackSchema)
