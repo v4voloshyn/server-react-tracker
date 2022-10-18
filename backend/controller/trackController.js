8644e96a00fc176c89fc9b4c7c8cf98d8aeaef2e
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-10-19 01:15:06 +0300

    FEAT: handle deleting and changing status of tracks

--- backend/controller/trackController.js
+++ backend/controller/trackController.js
@@ -1,42 +1,95 @@
 import asyncHandler from 'express-async-handler';
-import TrackModel from '../models/trackModel.js'
+import TrackModel from '../models/trackModel.js';
 
 // @desc Get all tracks
 // @route GET /api/v1/tracks
 // @acces Public
-export const getTracks = asyncHandler (async (req, res) => {
+export const getTracks = asyncHandler(async (req, res) => {
 	const allTracks = await TrackModel.find({});
 	res.status(200).json(allTracks);
-})
+});
 
 // @desc START/Create new track
 // @route POST /api/v1/tracks
 // @acces Public
-export const createNewTrack = asyncHandler (async (req, res) => {
+export const createNewTrack = asyncHandler(async (req, res) => {
 	const track = await TrackModel.create({
-		title: req.body.title || new Date().toISOString()
-	})
+		title: req.body.title || new Date().toISOString(),
+	});
 	return res.status(201).json(track);
-})
+});
 
 // @desc Delete track
 // @route DELETE /api/v1/tracks/:trackID
 // @acces Public
-export const deleteTrack = asyncHandler (async (req, res) => {
-	const track = await TrackModel.findByIdAndDelete(req.params.trackID)
-	if(!track){
-		return res.status(404).json({sucess: false, msg: 'Wrong trackID or it is removed already'});
+export const deleteTrack = asyncHandler(async (req, res) => {
+	const track = await TrackModel.findByIdAndDelete(req.params.trackID);
+	if (!track) {
+		return res.status(404).json({
+			success: false,
+			msg: 'Wrong trackID or it is removed already',
+		});
 	}
-	res.status(201).json({sucess: true, msg: 'Track successfull was removed', track});
-})
+	res.status(201).json({
+		success: true,
+		msg: 'Track successfull was removed',
+		track,
+	});
+});
 
-// @desc Pause track
+// @desc Delete ALL tracks
+// @route DELETE /api/v1/tracks
+// @acces Public
+export const deleteALLTracks = asyncHandler(async (req, res) => {
+	await TrackModel.deleteMany({});
+	res.status(200).json({
+		success: true,
+		msg: 'ALL tracks was deleted',
+	});
+});
+
+// @desc Change track status (Play/Pause)
 // @route PUT /api/v1/tracks/:trackID
 // @acces Public
-export const changeTrackStatus = asyncHandler (async (req, res) => {
-	const track = await TrackModel.findById(req.params.trackID)
-	if(!track){
-		return res.status(404).json({sucess: false, msg: 'Track does not exist'});
+export const changeTrackStatus = asyncHandler(async (req, res) => {
+	const { trackID } = req.params;
+	const track = await TrackModel.findById(trackID);
+	if (!track) {
+		return res.status(404).json({
+			success: false,
+			msg: 'Track does not exist',
+		});
 	}
-	//res.status(201).json({sucess: true, msg: 'Track successfull was removed', track});
-})
+	if (track.isPaused) {
+		const updatedTrack = await TrackModel.findByIdAndUpdate(
+			trackID,
+			{
+				isPaused: false,
+			},
+			{
+				new: true,
+			}
+		);
+		return res.status(200).json({
+			success: true,
+			msg: 'Unpause track',
+			updatedTrack,
+		});
+	} else {
+		const updatedTrack = await TrackModel.findByIdAndUpdate(
+			trackID,
+			{
+				isPaused: true,
+				secondsCount: track.secondsCount + (Date.now() - new Date(track['updatedAt'])) / 1000,
+			},
+			{
+				new: true,
+			}
+		);
+		return res.status(200).json({
+			success: true,
+			msg: 'Track was paused',
+			updatedTrack,
+		});
+	}
+});
--- backend/router/trackRouter.js
+++ backend/router/trackRouter.js
@@ -1,9 +1,16 @@
-import express from "express";
-import { changeTrackStatus, createNewTrack, deleteTrack, getTracks } from "../controller/trackController.js";
+import express from 'express';
+import {
+	changeTrackStatus,
+	createNewTrack,
+	deleteALLTracks,
+	deleteTrack,
+	getTracks,
+} from '../controller/trackController.js';
 
 export const trackRouter = express.Router();
 
 trackRouter.get('/', getTracks);
 trackRouter.post('/', createNewTrack);
+trackRouter.delete('/', deleteALLTracks);
 trackRouter.delete('/:trackID', deleteTrack);
 trackRouter.put('/:trackID', changeTrackStatus);
